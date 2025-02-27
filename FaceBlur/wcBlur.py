b78ec83b4400e4d831754511a365fa96562ac05d
theCarterDavis <carter@davisfamily.org>
2025-02-27 11:19:10 -0800

    Text detection basics

--- FaceBlur/wcBlur.py
+++ FaceBlur/wcBlur.py
@@ -1,19 +1,20 @@
 import cv2
 import mediapipe as mp
-
+#capturing video feed
 cam = cv2.VideoCapture(0)
 
 while True:
     ret, img = cam.read()
-
+    #Message snet if the feed is not being recieved
     if not ret or img is None:
         print("Failed to capture image from webcam")
         continue  # Skip the rest of the loop and try agai
 
+    #Flipping the immage to look mirroed
     img = cv2.flip(img, 1)
 
     H, W, x = img.shape
-
+    #using mediapipe to locate the faces that may be in the immage
     face = mp.solutions.face_detection
 
     # Model Selection 0 is for close up, 1 is for far away
@@ -22,19 +23,21 @@ while True:
         out = face_detection.process(img_rgb)
 
         if out.detections is not None:
-            for detection in out.detections:
+            for detection in out.detections:#looping through each detection made
                 loc = detection.location_data
 
                 bb = loc.relative_bounding_box
-
+                #Retreving the bounding boxes that are fromthe detections that are returned
                 x1, y1, w, h = bb.xmin, bb.ymin, bb.width, bb.height
 
+                #getting the locations so that the fave can be blured propery
                 x1 = int(x1 * W)
                 y1 = int(y1 * H)
                 w = int(W * w)
                 h = int(H * h)
                 # img = cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(0,255,0),2)
 
+                #applying a bluring affect to the bounding boxes that were retrieved
                 img[y1:y1 + h, x1:x1 + w] = cv2.blur(img[y1:y1 + h, x1:x1 + w], (35, 35))
 
     cv2.imshow('Webcam', img)
--- OpenCV-Webcam.py
+++ OpenCV-Webcam.py
@@ -1,10 +1,11 @@
 import cv2
-
-cam = cv2.VideoCapture(0)
+#capturing the video feef from my laptop
+cam = cv2.VideoCapture(0)#0 is the default camera
 
 while True:
     ret, frame = cam.read()
     cv2.imshow('Webcam', frame)
+    #Looping the campera feed/displaying it untill and interupt or q is peressed
     if cv2.waitKey(40) & 0xFF == ord('q'):
         break
 
